47f4af6e57f79b94ba1bad627a6c2c06cbb7d0b5
priyanka-shilpi820 <priyankashilpi@sidmectech.in>
2025-07-02 09:46:16 +0530

    new

--- spare_management_system/models/spare.py
+++ spare_management_system/models/spare.py
@@ -10,3 +10,11 @@ class Spare(models.Model):
     part_number = fields.Integer( string='CODE', required=True)
     part_name = fields.Char(string='Part Name')
     description = fields.Char(string='Description')
+    model_name=fields.Many2one("vehicle.info","Vehicle")
+    unit_of_measure=fields.Integer("UOM")
+    cost_price=fields.Integer("Cost Price")
+    sale_price=fields.Integer("Sales Price")
+    whole_sale_price=fields.Integer("Whole SalePrice")
+    image=fields.Binary("Image")
+
+
