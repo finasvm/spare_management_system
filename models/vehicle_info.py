2fa2b5e3200414b30bba9513f6f215c02fe664b5
krishnavenisidmec <krishnaveni@sidmectech.in>
2025-07-02 11:05:28 +0530

    saved

--- models/vehicle_info.py
+++ models/vehicle_info.py
@@ -11,5 +11,23 @@ class VehicleInformation(models.Model):
     type=fields.Char("Vehicle Type")
     make=fields.Char("Make")
     model=fields.Char("Model")
-    year=fields.Date("Year")
+    # year=fields.Integer("Year")
+    model_year=fields.Integer("Year")
+    order_lines = fields.One2many("vehicle.order.lines", "vehicle", "Order lines")
+
+
+
+class VehicleorderLines(models.Model):
+    _name="vehicle.order.lines"
+
+
+    product_id=fields.Many2one("product.product","product Name")
+    quantity=fields.Integer("qty")
+    unit_price=fields.Float("Unitprice")
+    vehicle=fields.Many2one("vehicle.information","Vehicle")
+    total = fields.Integer("subtotal",compute="compute_sub_total")
+
+    def compute_sub_total(self):
+        for rec in self:
+            rec.total=rec.quantity * rec.unit_price
 
